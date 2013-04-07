ae287efd2d05c7a6034c0e0db97e2268e05de67b
hashiz <hashiz@meridiani.jp>
2013-04-08 02:36:05 +0900

    worked

--- src/jp/meridiani/apps/wificonnect/EditActivity.java
+++ src/jp/meridiani/apps/wificonnect/EditActivity.java
@@ -8,19 +8,24 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 import jp.meridiani.apps.wificonnect.Constants;
 
-public class EditActivity extends Activity {
+public class EditActivity extends Activity implements OnItemSelectedListener, OnItemClickListener {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		Bundle bundle = getIntent().getBundleExtra(com.twofortyfouram.locale.Intent.EXTRA_BUNDLE);
 		setContentView(R.layout.activity_edit);
+
+		Bundle bundle = getIntent().getBundleExtra(com.twofortyfouram.locale.Intent.EXTRA_BUNDLE);
 
 		String ssid = "";
 		if (bundle != null) {
@@ -44,13 +49,16 @@ public class EditActivity extends Activity {
 		wifiListView.setAdapter(adapter);
 		wifiListView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
 		wifiListView.setItemChecked(selPos, true);
+		wifiListView.setOnItemSelectedListener(this);
+		wifiListView.setOnItemClickListener(this);
 	}
 
 	@Override
     public void finish()
     {
 		ListView wifiListView = (ListView)findViewById(R.id.SSIDList);
-		String ssid = (String)wifiListView.getSelectedItem();
+		int selPos = wifiListView.getCheckedItemPosition();
+		String ssid = (String)wifiListView.getAdapter().getItem(selPos);
 
         if (ssid != null && ssid.length() > 0)
         {
@@ -58,10 +66,23 @@ public class EditActivity extends Activity {
             Bundle resultBundle = new Bundle();
             resultBundle.putString(Constants.BUNDLE_AP_SSID, ssid);
 
+            resultIntent.putExtra(com.twofortyfouram.locale.Intent.EXTRA_STRING_BLURB, ssid);
             resultIntent.putExtra(com.twofortyfouram.locale.Intent.EXTRA_BUNDLE, resultBundle);
 
             setResult(RESULT_OK, resultIntent);
         }
         super.finish();
     }
+
+	@Override
+	public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+	}
+
+	@Override
+	public void onNothingSelected(AdapterView<?> parent) {
+	}
+
+	@Override
+	public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+	}
 }
--- src/jp/meridiani/apps/wificonnect/FireReceiver.java
+++ src/jp/meridiani/apps/wificonnect/FireReceiver.java
@@ -5,39 +5,87 @@ import java.util.List;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.wifi.SupplicantState;
 import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.widget.Toast;
 import jp.meridiani.apps.wificonnect.Constants;
 
 public class FireReceiver extends BroadcastReceiver {
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
-        if (!com.twofortyfouram.locale.Intent.ACTION_QUERY_CONDITION.equals(intent.getAction())) {
+		long starttime;
+
+		if (!com.twofortyfouram.locale.Intent.ACTION_FIRE_SETTING.equals(intent.getAction())) {
         	return;
         }
-        Bundle bundle = intent.getExtras();
+        Bundle bundle = intent.getBundleExtra(com.twofortyfouram.locale.Intent.EXTRA_BUNDLE);
         if (bundle == null) {
         	return;
         }
-        String ssid = (String)bundle.get(Constants.BUNDLE_AP_SSID);
-        if (ssid == null) {
+        String ssid = bundle.getString(Constants.BUNDLE_AP_SSID);
+        if (ssid == null || ssid.length() < 1) {
         	return;
         }
         WifiManager wifi = (WifiManager)context.getSystemService(Context.WIFI_SERVICE);
         List<WifiConfiguration>wifiList = wifi.getConfiguredNetworks();
+        int currentId = wifi.getConnectionInfo().getNetworkId();
         for (WifiConfiguration wifiConf : wifiList) {
         	if (ssid.equals(wifiConf.SSID)) {
-        		// connect to ssid
-        		if (wifi.enableNetwork(wifiConf.networkId,false)) {
-        			// success
+        		if (wifiConf.status == WifiConfiguration.Status.CURRENT) {
+            		Toast.makeText(context, "Already connect " + ssid, Toast.LENGTH_LONG).show();
+            		return;
         		}
-        		else {
-        			// failure
-        		}
+        		currentId = wifiConf.networkId;
         		break;
         	}
+        }
+		Toast.makeText(context, "Connect to " + ssid, Toast.LENGTH_LONG).show();
+
+		// disconnect current network
+		wifi.disconnect();
+
+		// wait disconnect
+        starttime = System.currentTimeMillis();
+        while (wifi.getConnectionInfo().getSupplicantState() != SupplicantState.DISCONNECTED) {
+        	if (System.currentTimeMillis() - starttime > 10 * 1000) {
+        		// time out
+        		break;
+        	}
+        	try {
+				Thread.sleep(500);
+			}
+        	catch (InterruptedException e) {
+			}
+        }
+
+        // disable other networks
+        wifi.enableNetwork(currentId, true);
+
+        wifi.reconnect();
+
+        // wait connect
+        starttime = System.currentTimeMillis();
+        while (wifi.getConnectionInfo().getSupplicantState() != SupplicantState.COMPLETED) {
+        	if (System.currentTimeMillis() - starttime > 10 * 1000) {
+        		break;
+        	}
+        }
+
+        if (wifi.getConnectionInfo().getNetworkId() == currentId) {
+	        // success
+			Toast.makeText(context, "Connected " + ssid, Toast.LENGTH_LONG).show();
+        }
+        else {
+			// failure
+			Toast.makeText(context, "Can't connect " + ssid, Toast.LENGTH_LONG).show();
+        }
+        // set all enable
+        for (WifiConfiguration wifiConf : wifiList) {
+        	wifi.enableNetwork(wifiConf.networkId, false);
         }
 	}
 }
