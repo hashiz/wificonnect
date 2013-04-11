c51f046cf685bb76e112322e6e8817c4e074b4a3
hashiz <hashiz@meridiani.jp>
2013-04-11 18:57:25 +0900

    * add WiFi on/off toggle * add Select and Cancel button

--- src/jp/meridiani/apps/wificonnect/activity/EditActivity.java
+++ src/jp/meridiani/apps/wificonnect/activity/EditActivity.java
@@ -11,58 +11,95 @@ import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.ListView;
+import android.widget.ToggleButton;
 
 public class EditActivity extends Activity implements OnItemSelectedListener, OnItemClickListener {
+
+	private String mSelectedSSID;
+	private ListView mWifiListView;
+	private ToggleButton mWifiButton;
+	private Button mSelectButton;
+	private Button mCancelButton;
+	private boolean mCanceled;
+	private boolean mWifiEnabledFirst;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		setContentView(R.layout.activity_edit);
+		// receive intent and extra data
+		Intent intent = getIntent();
+		if (com.twofortyfouram.locale.Intent.ACTION_EDIT_SETTING.equals(intent.getAction())) {
+			super.finish();
+			return;
+		}
 
 		Bundle bundle = getIntent().getBundleExtra(com.twofortyfouram.locale.Intent.EXTRA_BUNDLE);
 
-		String ssid = "";
+		mSelectedSSID = null;
 		if (bundle != null) {
-			ssid = bundle.getString(Constants.BUNDLE_AP_SSID);
+			mSelectedSSID = bundle.getString(Constants.BUNDLE_AP_SSID);
+		}
+		if (mSelectedSSID == null) {
+			mSelectedSSID = "";
 		}
 
-		WifiManager wifi = (WifiManager)getSystemService(Context.WIFI_SERVICE);
-		List<WifiConfiguration>wifiConfList = wifi.getConfiguredNetworks();
+		mCanceled = false;
 
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-							android.R.layout.simple_list_item_single_choice);
-		int selPos = 0;
-		for ( WifiConfiguration wifiConf : wifiConfList) {
-			adapter.add(wifiConf.SSID);
-			if (ssid.equals(wifiConf.SSID)) {
-				selPos = adapter.getCount() - 1;
+		// set view
+		setContentView(R.layout.activity_edit);
+		mWifiListView = (ListView)findViewById(R.id.SSIDList);
+		mWifiButton = (ToggleButton)findViewById(R.id.wifi_button);
+		mSelectButton = (Button)findViewById(R.id.select_button);
+		mCancelButton = (Button)findViewById(R.id.cancel_button);
+
+		WifiManager wifi = getWifiManager();
+		mWifiEnabledFirst = wifi.isWifiEnabled();
+
+		mWifiButton.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView,
+					boolean isChecked) {
+				setWifiEnable(isChecked);
 			}
-		}
+		});
 
-		ListView wifiListView = (ListView)findViewById(R.id.SSIDList);
-		wifiListView.setAdapter(adapter);
-		wifiListView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
-		wifiListView.setItemChecked(selPos, true);
-		wifiListView.setOnItemSelectedListener(this);
-		wifiListView.setOnItemClickListener(this);
+		mWifiButton.setChecked(wifi.isWifiEnabled());
+
+		mSelectButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				onSelectClick((Button)v);
+			}
+		});
+		mCancelButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				onCancelClick((Button)v);
+			}
+		});
 	}
 
 	@Override
     public void finish()
     {
-		ListView wifiListView = (ListView)findViewById(R.id.SSIDList);
-		int selPos = wifiListView.getCheckedItemPosition();
-		String ssid = (String)wifiListView.getAdapter().getItem(selPos);
+		int selPos = mWifiListView.getCheckedItemPosition();
+		String ssid = null;
+		if (selPos >= 0) {
+			ssid = (String)mWifiListView.getAdapter().getItem(selPos);
+		}
 
-        if (ssid != null && ssid.length() > 0)
-        {
-            Intent resultIntent = new Intent();
+        Intent resultIntent = new Intent();
+        if (! mCanceled && ssid != null && ssid.length() > 0) {
             Bundle resultBundle = new Bundle();
             resultBundle.putString(Constants.BUNDLE_AP_SSID, ssid);
 
@@ -71,7 +108,14 @@ public class EditActivity extends Activity implements OnItemSelectedListener, On
 
             setResult(RESULT_OK, resultIntent);
         }
-        super.finish();
+        else {
+            setResult(RESULT_CANCELED, resultIntent);
+        }
+        WifiManager wifi = getWifiManager();
+        if (!mWifiEnabledFirst && wifi.isWifiEnabled()) {
+        	wifi.setWifiEnabled(false);
+        }
+    	super.finish();
     }
 
 	@Override
@@ -84,5 +128,49 @@ public class EditActivity extends Activity implements OnItemSelectedListener, On
 
 	@Override
 	public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+	}
+
+	private void setWifiEnable(boolean enabled) {
+		getWifiManager().setWifiEnabled(enabled);
+		if (enabled) {
+			updateWifiList();
+		}
+	}
+
+	private WifiManager getWifiManager() {
+		return (WifiManager)getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+	}
+
+	private void updateWifiList() {
+		WifiManager wifi = getWifiManager();
+
+		List<WifiConfiguration>wifiConfList = wifi.getConfiguredNetworks();
+
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+							android.R.layout.simple_list_item_single_choice);
+		int selPos = 0;
+		for ( WifiConfiguration wifiConf : wifiConfList) {
+			adapter.add(wifiConf.SSID);
+			if (mSelectedSSID.equals(wifiConf.SSID)) {
+				selPos = adapter.getCount() - 1;
+			}
+		}
+
+		ListView wifiListView = (ListView)findViewById(R.id.SSIDList);
+		wifiListView.setAdapter(adapter);
+		wifiListView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+		wifiListView.setItemChecked(selPos, true);
+		wifiListView.setOnItemSelectedListener(this);
+		wifiListView.setOnItemClickListener(this);
+	}
+
+	private void onSelectClick(Button b) {
+		mCanceled = false;
+		finish();
+	}
+
+	private void onCancelClick(Button b) {
+		mCanceled = true;
+		finish();
 	}
 }
